af861c30ec862f9ad33da15465ca2defcbea4e9e
min0712 <47189845+min0712@users.noreply.github.com>
2022-06-24 10:23:32 -0400

    Update ch03_prac.py

--- ch03/ch03_prac.py
+++ ch03/ch03_prac.py
@@ -2,9 +2,18 @@
 
 secret_word = "giraffe"
 guess = ""
+count = 0
+guess_limit = 3
+out_of_guesses = False
 
-while guess != secret_word:
-  guess = input("Enter guess: ")
+while guess != secret_word and not(out_of_guesses):
+  if count < guess_limit:
+    guess = input("Enter guess: ")
+    count += 1
+  else:
+    out_of_guesses = True
   
-print("You win!")
-
+if out_of_guesses:
+  print("You lose")
+else:
+  print("You win")
